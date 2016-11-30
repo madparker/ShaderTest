e9cc273c9f589c977e535d1c1ec5741e2daa585d
Matthew Parker <maddparker@gmail.com>
2016-11-30 15:31:44 -0500

    Init Commit

---
+++ ShaderTest/Assets/ShaderTests/ModShaderTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModShaderTest : MonoBehaviour {
+
+	Material mat;
+
+	float size = 0;
+
+	// Use this for initialization
+	void Start () {
+		mat = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		size += Time.deltaTime;
+		mat.SetFloat("_Radius", 8 + Mathf.Sin(size) * 5);
+		mat.SetVector("_Center", 
+			new Vector3(Mathf.Sin(size) * 5,
+				0,
+				Mathf.Cos(size) * 5));
+	}
+}
