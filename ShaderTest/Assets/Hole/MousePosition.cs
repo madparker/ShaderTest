8dd83ad1bebca5929247497ad5a70d5d0345fbae
Matthew Parker <maddparker@gmail.com>
2016-12-07 16:43:22 -0500

    Added a Hole

---
+++ ShaderTest/Assets/Hole/MousePosition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePosition : MonoBehaviour {
+
+	Material mat;
+
+	float size = 0;
+
+	// Use this for initialization
+	void Start () {
+		mat = GetComponent<Renderer>().material;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+		mouseWorldPos.z = Camera.main.nearClipPlane;
+
+		print("mousePos: " + Input.mousePosition);
+
+		mat.SetVector("_MousePos", mouseWorldPos);
+	}
+}
